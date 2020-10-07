3ac8fce59f4b58c60a6532abdb0992a649e88a7f
iradbouzidi <irad.bouzidi@esprit.tn>
2020-10-07 20:27:14 +0100

    Build the product component

---
+++ src/Home.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+
+function Home() {
+  return (
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/gateway/placement/launch/ThewalkingdeadworldbeyondS1/WDWB_S1_GWBleedingHero_FT_COVIDUPDATE_XSite_1500X600_PV_en-GB._CB403059520_.jpg"
+        alt=""
+      />
+
+      {/* Product: id, title, price, rating, image */}
+      <div className="home__row">
+        <Product
+          id="12345678"
+          title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+        />
+        <Product
+          id="12345678"
+          title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+        />
+      </div>
+
+      <div className="home__row">
+        <Product
+          id="12345678"
+          title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+        />
+        <Product
+          id="12345678"
+          title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+        />
+        <Product
+          id="12345678"
+          title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id="12345678"
+          title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/StateProvider.js
@@ -0,0 +1,10 @@
+// Setup data layer
+// We need this to track the basket
+
+import React, { createContext, useContext, useReducer } from "react";
+
+// This is the data layer
+export const StateContext = createContext();
+
+// Build a provider
+export const StateProvider = { reduce, initialState, children };
